af62cf0b3d86f8ed4d79e1b4b2f421a3894589ee
CodeNext <codenext@admins-MBP.lan>
2018-11-17 15:54:08 -0500

    fun

--- functions1.js
+++ functions1.js
@@ -1,18 +1,39 @@
 const READLINE = require("readline-sync");
-
-// **** Problem 1: printMessage() ****
+que = READLINE.question
+show = console.log
+name = que("What is your name?")
+ show("**** Problem 1: printMessage() ****")
 // Write a function that prints any message you want.
+printMessage = function(){
+	show(`Whats up ${name}.`)
+}
+printMessage()
 
-
-// **** Problem 2: printFiveMessages() ****
+show("**** Problem 2: printFiveMessages() ****")
 // Write a function that calls printMessage() five times.
+printMessage5 = function(){
+	printMessage()
+	printMessage()
+	printMessage()
+	printMessage()
+	printMessage()
+}
+printMessage5()
 
-
-// **** Problem 3: getUserInput() ****
+show("**** Problem 3: getUserInput() ****")
 // Write a function that asks the user if they'd like to print your message
 // once or five times. Then call one of the two functions above based on
 // what the user decides.
-
+man = function(){
+	so = que("Would you like to print my message once or 5 times?")
+	if(so.includes("1") || so.includes("on")){
+		printMessage()
+	}
+	if(so.includes("5")|| so.includes("five")){
+		printMessage5()
+	}
+}
+man()
 
 // **** Problem 4: printGreeting() ****
 // Write a function that prints a greeting message to the user.
